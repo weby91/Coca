1ddb2bd18c80ef38ed296f10d688740647fe6de1
webyjulo <webster@julofinance.com>
2018-07-11 22:56:44 +0700

    - Add onClickListener - Define and Call incrementNum() function on button click and show the result to TextView

--- app/src/main/java/coca/webster/com/coca/MainActivity.java
+++ app/src/main/java/coca/webster/com/coca/MainActivity.java
@@ -2,11 +2,12 @@ package coca.webster.com.coca;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
@@ -16,14 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        final TextView tv = findViewById(R.id.sample_text);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(String.valueOf(incrementNum()));
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public native int incrementNum();
 }
